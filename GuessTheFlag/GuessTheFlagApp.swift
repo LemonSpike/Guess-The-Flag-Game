9511cb7413f28c37dbd75b2ab1b989c136f4b139
Pranav Kasetti <pranavkasetti@gmail.com>
2020-11-21 17:48:39 +0000

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Pranav Kasetti on 21/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
